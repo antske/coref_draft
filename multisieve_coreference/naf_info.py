95cbaeedd6998e489b600b471e7a7fef88010e10
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-12 15:16:34 +0200

    Move definition of `CquotationNaf` and `Cconstituent_information`

--- multisieve_coreference/naf_info.py
+++ multisieve_coreference/naf_info.py
@@ -4,118 +4,13 @@ from collections import defaultdict
 from KafNafParserPy import Cspan, Ctarget, Ccoreference
 
 from .mention_data import Cmention, Cquotation
+from .naf_classes import Cconstituent_information, CquotationNaf
 
 
 dep_extractor = None
 head2deps = defaultdict(list)
 dep2heads = defaultdict(list)
 stop_words = []
-
-
-class CquotationNaf:
-    '''
-    This class encodes source and quotation
-    '''
-
-    def __init__(self):
-        '''
-        Constructor for quotations object
-        '''
-        self.sip = ''
-        self.span = []
-        self.source = []
-        self.addressee = []
-        self.topic = []
-        self.beginquote = ''
-        self.endquote = ''
-
-    def add_span(self, span):
-
-        self.span = span
-
-    def add_span_id(self, span_id):
-
-        self.span.append(span_id)
-
-
-class Cconstituent_information:
-    '''
-    This class contains the main constructional information of mentions
-    '''
-
-    def __init__(self, head_id, span=None):
-        '''
-        Constructor for the constituent object
-        :param head_id: term id of the head of the constituent
-        :param span: list of term ids providing full span of hte constituent
-        '''
-        self.head_id = head_id
-        self.span = span if span is not None else []
-        self.multiword = []
-        self.modifiers = []
-        self.appositives = []
-        self.predicatives = []
-        self.etype = ''
-
-    def set_span(self, span):
-
-        self.span = span
-
-    def get_span(self):
-
-        return self.span
-
-    def set_multiword(self, mw):
-
-        self.multiword = mw
-
-    def get_multiword(self):
-
-        return self.multiword
-
-    def set_modifiers(self, mods):
-
-        self.modifiers = mods
-
-    def add_modifier(self, mod):
-
-        self.modifiers.append(mod)
-
-    def get_modifiers(self):
-
-        return self.modifiers
-
-    def set_appositives(self, apps):
-
-        self.appositives = apps
-
-    def add_appositive(self, app):
-
-        self.appositives.append(app)
-
-    def get_appositives(self):
-
-        return self.appositives
-
-    def set_predicatives(self, preds):
-
-        self.predicatives = preds
-
-    def add_predicative(self, pred):
-
-        self.predicatives.append(pred)
-
-    def get_predicatives(self):
-
-        return self.predicatives
-
-    def set_etype(self, etype):
-
-        self.etype = etype
-
-    def get_etype(self):
-
-        return self.etype
 
 
 def get_constituent(head):
