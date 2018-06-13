31ae4921d9dd89c5fea13adcd66f7ec81e855688
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-13 13:42:48 +0200

    Fix copy paste

--- multisieve_coreference/constituency_tree.py
+++ multisieve_coreference/constituency_tree.py
@@ -3,10 +3,13 @@ class ConstituencyTree:
     Groups together information from a constituency tree and exposes convenient
     lookups.
     """
-    def __init__(nafobj, item_filter):
+    def __init__(self, nafobj, item_filter):
         # Create all dicts
         # self.
-        raise NotImplementedError()
+        self.head2deps, self.dep2heads = self.create_headdep_dicts(
+            nafobj,
+            item_filter
+        )
 
     def direct_dependents(self, ID):
         """
@@ -45,25 +48,26 @@ class ConstituencyTree:
         """
         raise NotImplementedError()
 
+    @staticmethod
     def create_headdep_dicts(
             nafobj,
             term_filter=lambda naf, t: naf.get_term(t).get_pos() != 'punct'):
-        '''
-        Function that creates dictionaries of dep to heads and head to deps
-        :param nafobj: nafobj from input file
-        :return: None
-        '''
+        """
+        Create dictionaries of dependent to heads and head to direct dependent
 
-        allhead2deps = defaultdict(list)
-        # To make sure we get a KeyError if something goes (horribly) wrong
-        dep2headIDs = dict()
+        :param nafobj:  nafobj from input file
+        :return:        head2deps, dep2heads
+        """
+
+        allhead2deps = {}
+        dep2headIDs = {}
         for dep in nafobj.get_dependencies():
             headID = dep.get_from()
             toID = dep.get_to()
             allhead2deps[headID].append((toID, dep.get_function()))
             dep2headIDs.setdefault(toID, []).append(headID)
 
-        global head2deps
+        head2deps = {}
         for headID, deps in allhead2deps.items():
             # I don't have to do something with the deps that are filtered out,
             # because if they are leaves they can just be deleted and if they
@@ -83,7 +87,9 @@ class ConstituencyTree:
                         head2deps.setdefault(super_headID, set()).update(deps)
 
         # Create the reverse too
-        global dep2heads
+        dep2heads = {}
         for headID, deps in head2deps.items():
             for toID, relation in deps:
                 dep2heads.setdefault(toID, set()).add((headID, relation))
+
+        return head2deps, dep2heads
