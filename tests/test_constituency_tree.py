659ea67fb1e06ee779f98494584a1ad48bafe711
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-14 11:38:33 +0200

    Update test

--- tests/test_constituency_tree.py
+++ tests/test_constituency_tree.py
@@ -9,12 +9,15 @@ def test_no_filter(naf_object, caplog):
     # <!--hd/obj1(herkende, zichzelf)-->
     # <dep from="t_1" to="t_2" rfunc="hd/obj1" />
     tree = ConstituencyTree(naf_object)
-    assert len(tree.head2deps) == 1
-    assert len(tree.dep2heads) == 2   # t_0 and t_2
-    assert len(tree.get_constituent('t_1')) == 3    # all three
-    assert len(tree.get_constituent('t_0')) == 1
-    assert len(tree.get_constituent('t_2')) == 1
-    assert len(tree.get_constituent('very random')) == 1
+    assert tree.head2deps == {'t_1': {('t_0', 'hd/su'), ('t_2', 'hd/obj1')}}
+    assert tree.dep2heads == {
+        't_0': {('t_1', 'hd/su')},
+        't_2': {('t_1', 'hd/obj1')}
+    }
+    assert tree.get_constituent('t_1') == {'t_0', 't_1', 't_2'}
+    assert tree.get_constituent('t_0') == {'t_0'}
+    assert tree.get_constituent('t_2') == {'t_2'}
+    assert tree.get_constituent('very random') == {'very random'}
 
 
 # def test_punct_filter(naf_object):
