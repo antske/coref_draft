0ff14d6cb074524b68a71125b30cec7108f25790
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-26 13:55:51 +0200

    Style

--- multisieve_coreference/resolve_coreference.py
+++ multisieve_coreference/resolve_coreference.py
@@ -205,16 +205,16 @@ def clean_up_coref_classes(coref_classes, mentions):
     grouped_merge_list = merge_merge_list(to_merge)
 
     for merges in grouped_merge_list:
-        overall_id = sorted(merges)[0]
-        if not coref_classes.get(overall_id) is None:
+        overall_id = min(merges)
+        if coref_classes.get(overall_id) is not None:
             for cid in merges:
                 if cid != overall_id:
-                    #FIXME; check when this is happening and whether it leads to missing coreferences
-                    if not coref_classes.get(cid) is None:
+                    # FIXME; check when this is happening and whether it leads to missing coreferences
+                    if coref_classes.get(cid) is not None:
                         coref_classes[overall_id] = coref_classes[overall_id] | coref_classes.get(cid)
                         coref_classes[cid] = None
             new_set = coref_classes.get(overall_id)
-        #reset mention coreference class to selected id
+        # reset mention coreference class to selected id
             for mention_id in new_set:
                 my_mention = mentions.get(mention_id)
                 my_mention.in_coref_class = [overall_id]
