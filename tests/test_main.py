ad8c2ff203a3f228e757fd448f6c00714e46d88c
Martin van Harmelen <Martin@vanharmelen.com>
2018-06-28 13:54:32 +0200

    Improved test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,9 @@ import subprocess
 import pytest
 
 
+from multisieve_coreference.resolve_coreference import process_coreference
+
+
 def test_main_example(example_naf_file, temp_file, example_naf_output):
     with open(example_naf_file) as fd, open(temp_file, 'w') as out:
         subprocess.check_call(
@@ -18,23 +21,24 @@ def test_main_example(example_naf_file, temp_file, example_naf_output):
         assert out.read() == correct.read()
 
 
-@pytest.mark.slow
-def test_main_sonar1(sonar_naf_file1, temp_file):
-    with open(sonar_naf_file1) as fd, open(temp_file, 'w') as out:
-        subprocess.check_call(
-            [sys.executable, '-m', 'multisieve_coreference'],
-            stdin=fd,
-            stdout=out
-        )
-    # No gold output available
+def test_example(example_naf_object, caplog):
+    # caplog.set_level('DEBUG')
+    caplog.set_level('WARNING', 'multisieve_coreference.constituency_tree')
+    out = process_coreference(example_naf_object)
+    assert out.coreference_layer is not None
 
 
 @pytest.mark.slow
-def test_main_sonar2(sonar_naf_file2, temp_file):
-    with open(sonar_naf_file2) as fd, open(temp_file, 'w') as out:
-        subprocess.check_call(
-            [sys.executable, '-m', 'multisieve_coreference'],
-            stdin=fd,
-            stdout=out
-        )
-    # No gold output available
+def test_main_sonar1(sonar_naf_object1, caplog):
+    # caplog.set_level('DEBUG')
+    caplog.set_level('WARNING', 'multisieve_coreference.constituency_tree')
+    out = process_coreference(sonar_naf_object1)
+    assert out.coreference_layer is not None
+
+
+@pytest.mark.slow
+def test_main_sonar2(sonar_naf_object2, caplog):
+    # caplog.set_level('DEBUG')
+    caplog.set_level('WARNING', 'multisieve_coreference.constituency_tree')
+    out = process_coreference(sonar_naf_object2)
+    assert out.coreference_layer is not None
