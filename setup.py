c822d3d14a28cd34dead6a4d26168f2a2ee8764e
Martin van Harmelen <Martin@vanharmelen.com>
2018-07-12 13:52:57 +0200

    Don't include LICENCE file in setup.py
    because it gets installed in a strange place and it is redundant
    because of the `License :: OSI Approved :: Apache Software License`
    classifier

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='multisieve_coreference',
     version='0.1.1',
@@ -18,10 +15,8 @@ setup(
     author='Antske Fokkens',
     author_email='antske.fokkens@vu.nl',
     url='https://github.com/antske/coref_draft/',
-    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     package_data={'multisieve_coreference': ['resources/*/*']},
-    data_files=[('', ['LICENSE'])],
     install_requires=[
         "KafNafParserPy>=1.88",
     ],
